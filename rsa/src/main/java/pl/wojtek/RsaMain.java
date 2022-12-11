29aa0ad6798779e646bdb16dd9b16c7aa967b21d
kopson99 <w.watral80@gmail.com>
2022-12-11 14:12:15 +0100

    no-message-attack

--- rsa/src/main/java/pl/wojtek/RsaMain.java
+++ rsa/src/main/java/pl/wojtek/RsaMain.java
@@ -6,11 +6,13 @@ import pl.wojtek.rsa.RSAUtil;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.security.*;
 import java.util.Base64;
+import java.util.Random;
 
 public class RsaMain {
-
 
 
     public static void main(String[] args) throws Exception {
@@ -22,33 +24,68 @@ public class RsaMain {
         final String message = "Wojtek Watral - WSB - 2022";
         System.out.println("Wiadomosc do zaszyfrowania: " + message);
         //encrypted -> kluczem prywatnym
-        String encryptedMessagePrivate = RSAUtil.encrypt(rsa.getPrivateKey(),message);
+        String encryptedMessagePrivate = RSAUtil.encrypt(rsa.getPrivateKey(), message);
         //Decryptec -> kluczem puvlicznym
-        String decryptedMessagePublic = RSAUtil.decrypt(rsa.getPublicKey(),encryptedMessagePrivate);
+        String decryptedMessagePublic = RSAUtil.decrypt(rsa.getPublicKey(), encryptedMessagePrivate);
 
         System.out.println("-----");
         System.out.println("Encrypted [PrivateKey] " + encryptedMessagePrivate);
         System.out.println("Decryptec [PublicKey] " + decryptedMessagePublic);
         System.out.println("-----");
         //encrypted -> kluczem publicznym
-        String encryptedMessagePublic = RSAUtil.encrypt(rsa.getPrivateKey(),message);
+        String encryptedMessagePublic = RSAUtil.encrypt(rsa.getPrivateKey(), message);
         //Decryptec -> kluczem prywatnym
-        String decryptedMessagePrivate = RSAUtil.decrypt(rsa.getPublicKey(),encryptedMessagePrivate);
+        String decryptedMessagePrivate = RSAUtil.decrypt(rsa.getPublicKey(), encryptedMessagePrivate);
         System.out.println("");
         System.out.println("Encrypted [PublicKey] " + encryptedMessagePublic);
         System.out.println("Decryptec [PrivateKey] " + decryptedMessagePrivate);
         System.out.println("-----");
 
 
-
-
-
-
-
-
-
+        noMessageAttack();
     }
 
 
+    private static void noMessageAttack() {
+        final int message = RandomUtil.getRandInt(0,100);
+        System.out.println("Wiadomość do zaszyforwania: " + message);
+        double c;
+        BigInteger decrypt;
+        int p = 5;
+        int q = 19;
+        int n = p * q;
+        int z = (p - 1) * (q - 1);
+        int d = 0;
+        int e;
+        for (e = 2; e < z; e++) {
+            if (getEZ(e, z) == 1) {
+                break;
+            }
+        }
+        for (int i = 0; i <= 9; i++) {
+            int x = 1 + (i * z);
+            if (x % e == 0) {
+                d = x / e;
+                break;
+            }
+        }
+        c = (Math.pow(message, e)) % n;
+        double s = (Math.pow(message, d)) % n;
+        double m = (Math.pow(s, e) % n);
+        System.out.println("m'" + m);
+        System.out.println("Encrypted: " + c);
+        BigInteger N = BigInteger.valueOf(n);
+        BigInteger C = BigDecimal.valueOf(c).toBigInteger();
+        decrypt = (C.pow(d)).mod(N);
+        System.out.println("Decryptec: " + decrypt);
 
+    }
+
+    private static int getEZ(int e, int z) {
+        if (e == 0) {
+            return z;
+        } else {
+            return getEZ(z % e, e);
+        }
+    }
 }
